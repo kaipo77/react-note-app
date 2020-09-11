9f5870915737aa674bb6f581edb68447dd20decb
sid <sid_ani21@yahoo.in>
2020-09-11 16:12:13 +0530

    finalized note app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,108 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import SidebarComponents from './sidebar/sidebar';
+import EditorComponents from './editor/editor';
+import SidebarItemComponents from './sidebaritem/sidebaritem';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const firebase = require('firebase');
+
+class App extends React.Component{
+
+  constructor(){
+    super();
+    this.state = {
+      selectedNoteIndex : null,
+      selectedNote : null,
+      notes : null
+    }
+  }
+  render(){
+    return(
+    <div className= 'app-container'>
+      <SidebarComponents 
+      selectedNoteIndex={this.state.selectedNoteIndex} 
+      notes ={this.state.notes} 
+      deleteNote = {this.deleteNote}
+      selectNote = {this.selectNote}
+      newNote = {this.newNote}>
+      </SidebarComponents>
+      {
+        this.state.selectedNote ?
+        <EditorComponents selectedNote={this.state.selectedNote}
+        selectedNoteIndex = {this.state.selectedNoteIndex}
+        notes={this.state.notes}
+        noteUpdate={this.noteUpdate}></EditorComponents> :
+        null
+      }
+    </div>);
+  }
+
+  componentDidMount = () => {
+    firebase.firestore().collection('notes').onSnapshot(serverUpdate => {
+      const notes = serverUpdate.docs.map(_doc =>{
+        const data = _doc.data();
+        data['id'] = _doc.id;
+        return data;
+      });
+      console.log(notes);
+      this.setState({ notes : notes});
+    });
+  }
+
+  selectNote =(note, index) => this.setState({
+    selectedNoteIndex : index, 
+    selectedNote : note
+  });
+
+  noteUpdate = (id, noteObj) => {
+    firebase.firestore().collection('notes').doc(id).update({
+      title : noteObj.title,
+      body : noteObj.body,
+      timestamp : firebase.firestore.FieldValue.serverTimestamp()
+    });
+  }
+
+  newNote = async (title) => {
+    const note = {
+      title : title,
+      body : ''
+    }
+    const newFromDB = await firebase.firestore().collection('notes').add({
+      title : note.title,
+      body : note.body,
+      timestamp : firebase.firestore.FieldValue.serverTimestamp()
+    });
+    const newId = newFromDB.id;
+    await this.setState({
+      notes : [...this.state.notes, note]
+    });
+
+    const newNoteIndex = this.state.notes.indexOf(this.state.notes.filter(_note => _note.id === newId)[0]);
+    this.setState({ selectedNote : this.state.notes[newNoteIndex], selectedNoteIndex : newNoteIndex});
+  }
+
+  deleteNote = async (note) => {
+    const noteIndex = this.state.notes.indexOf(note);
+    if(this.state.selectedNoteIndex === noteIndex){
+      this.setState({ selectedNoteIndex : null, selectedNote : null });
+    }
+    else{
+      if(this.state.notes.length > 1)
+      {
+      //this.selectNote(this.state.notes[this.state.selectedNoteIndex - 1], this.state.selectedNoteIndex - 1); 
+      this.setState({ selectedNoteIndex : this.state.selectedNoteIndex,
+      selectedNote : this.state.selectedNote});
+    } 
+      else
+      this.setState({ selectedNoteIndex : null, selectedNote : null });
+    }
+
+    firebase.firestore().collection('notes').doc(note.id).delete();
+    await this.setState({ notes : this.state.notes.filter(_note => _note !== note) });
+    
+  }
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,30 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const firebase = require('firebase');
+require('firebase/firestore');
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAHWQKKOfztwoqGV4sAhKVcL1jDUNwLHmI",
+  authDomain: "evernote-clone-ee74d.firebaseapp.com",
+  databaseURL: "https://evernote-clone-ee74d.firebaseio.com",
+  projectId: "evernote-clone-ee74d",
+  storageBucket: "evernote-clone-ee74d.appspot.com",
+  messagingSenderId: "1069799110474",
+  appId: "1:1069799110474:web:53e2be4da9196724f0b3b3",
+  measurementId: "G-M2ZL8FQ2CM"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('evernote-container')
 );
 
 // If you want your app to work offline and load faster, you can change
